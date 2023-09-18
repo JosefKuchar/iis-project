95eca816a2d783226a229ae4509ec3a4d1483002
Josef Kuchař <josef.kuchar267@gmail.com>
2023-09-18 16:08:29 +0200

    Hot reload, templates, tailwind

--- go.mod
+++ go.mod
@@ -4,10 +4,13 @@ go 1.21.1
 
 require (
 	github.com/go-chi/chi/v5 v5.0.10 // indirect
+	github.com/go-sql-driver/mysql v1.7.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/tmthrgd/go-hex v0.0.0-20190904060850-447a3041c3bc // indirect
 	github.com/uptrace/bun v1.1.16 // indirect
+	github.com/uptrace/bun/dialect/mysqldialect v1.1.16 // indirect
 	github.com/vmihailenco/msgpack/v5 v5.3.5 // indirect
 	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
+	golang.org/x/mod v0.12.0 // indirect
 	golang.org/x/sys v0.12.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,17 +1,41 @@
 package main
 
 import (
+	"database/sql"
+	"html/template"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
+	sqldb, err := sql.Open("mysql", "root:pass@/test")
+	if err != nil {
+		panic(err)
+	}
+
+	println(sqldb)
+
+	fs := http.FileServer(http.Dir("static"))
+
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
+	r.Handle("/static/*", http.StripPrefix("/static/", fs))
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("welcome"))
+		tmpl, err := template.ParseFiles("templates/index.html")
+		if err != nil {
+			panic(err)
+		}
+		tmpl.Execute(w, nil)
+	})
+	r.Post("/clicked", func(w http.ResponseWriter, r *http.Request) {
+		tmpl, err := template.ParseFiles("templates/clicked.html")
+		if err != nil {
+			panic(err)
+		}
+		tmpl.Execute(w, nil)
 	})
 	http.ListenAndServe(":3000", r)
 }
