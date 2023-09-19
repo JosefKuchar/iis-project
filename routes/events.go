0288f4e080d08ff1b4ccb8396a0287c4f1fe5458
S1ro1 <matej.sirovatka@gmail.com>
2023-09-19 15:39:08 +0200

    Add recursive query for categories

--- routes/events.go
+++ routes/events.go
@@ -22,11 +22,26 @@ func (rs resources) EventRoutes() chi.Router {
 	r.Get("/{id}", func(w http.ResponseWriter, r *http.Request) {
 		id := chi.URLParam(r, "id")
 		var event models.Event
-		err := rs.db.NewSelect().Model(&event).Where("event.id = ?", id).Relation("Location").Scan(r.Context())
+		err := rs.db.NewSelect().Model(&event).Where("event.id = ?", id).Relation("Location").Relation("Categories").Scan(r.Context())
 		if err != nil {
 			fmt.Println(err)
 		}
 
+
+		for _, category := range event.Categories {
+			var categories []models.Category
+			rs.db.NewRaw(
+				`WITH RECURSIVE children as (
+					SELECT * FROM categories c WHERE c.id = ?
+					UNION ALL
+					SELECT c2.* FROM categories as c2, children as ch
+					WHERE c2.id = ch.parent_id 
+				)
+				SELECT * FROM children
+				`, category.ID).Scan(r.Context(), &categories)
+			
+			fmt.Println(categories)
+		}
 		rs.tmpl.ExecuteTemplate(w, "page-event", event)
 	})
 
