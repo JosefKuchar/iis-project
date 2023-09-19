41ccd95f7183d7d5e2874fec1a015fff96493928
Josef Kuchař <josef.kuchar267@gmail.com>
2023-09-19 13:03:42 +0200

    Events and detail

--- routes/events.go
+++ routes/events.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"JosefKuchar/iis-project/cmd/models"
+	"fmt"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
@@ -16,6 +17,17 @@ func (rs resources) EventRoutes() chi.Router {
 		rs.db.NewSelect().Model(&events).Relation("Location").Scan(r.Context())
 
 		rs.tmpl.ExecuteTemplate(w, "page-events", events)
+	})
+
+	r.Get("/{id}", func(w http.ResponseWriter, r *http.Request) {
+		id := chi.URLParam(r, "id")
+		var event models.Event
+		err := rs.db.NewSelect().Model(&event).Where("Event.ID = ?", id).Relation("Location").Scan(r.Context())
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		rs.tmpl.ExecuteTemplate(w, "page-event", event)
 	})
 
 	return r
