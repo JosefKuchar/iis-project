2fdcdf54b7fd9ddf850328a6c7ea1e4f0b5698ff
Josef Kuchař <josef.kuchar267@gmail.com>
2023-11-27 18:32:55 +0100

    Unlimited capacity

--- cmd/db.go
+++ cmd/db.go
@@ -5,9 +5,7 @@ import (
 	"context"
 	"database/sql"
 	"os"
-	"time"
 
-	"github.com/brianvoe/gofakeit/v6"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/joho/godotenv"
 	"github.com/uptrace/bun"
@@ -48,89 +46,89 @@ func main() {
 	// Enable foreign keys
 	db.NewRaw("SET FOREIGN_KEY_CHECKS = 1").Exec(context.Background())
 
-	for i := 0; i < 10; i++ {
-		location := &models.Location{
-			Name:     gofakeit.Name(),
-			Street:   gofakeit.Street(),
-			Zip:      gofakeit.Zip(),
-			City:     gofakeit.City(),
-			Approved: true,
-		}
-		db.NewInsert().Model(location).Exec(ctx)
-	}
+	// for i := 0; i < 10; i++ {
+	// 	location := &models.Location{
+	// 		Name:     gofakeit.Name(),
+	// 		Street:   gofakeit.Street(),
+	// 		Zip:      gofakeit.Zip(),
+	// 		City:     gofakeit.City(),
+	// 		Approved: true,
+	// 	}
+	// 	db.NewInsert().Model(location).Exec(ctx)
+	// }
 
-	for i := 0; i < 10; i++ {
-		start := gofakeit.Date()
-		event := &models.Event{
-			Name:        gofakeit.Word(),
-			Description: gofakeit.LoremIpsumSentence(20),
-			Capacity:    int64(gofakeit.Number(1, 50)),
-			Start:       start,
-			End:         start.Add(time.Hour * time.Duration(gofakeit.Number(1, 10))),
-			LocationID:  int64(gofakeit.Number(1, 10)),
-			Approved:    true,
-		}
-		db.NewInsert().Model(event).Exec(ctx)
+	// for i := 0; i < 10; i++ {
+	// 	start := gofakeit.Date()
+	// 	event := &models.Event{
+	// 		Name:        gofakeit.Word(),
+	// 		Description: gofakeit.LoremIpsumSentence(20),
+	// 		Capacity:    int64(gofakeit.Number(1, 50)),
+	// 		Start:       start,
+	// 		End:         start.Add(time.Hour * time.Duration(gofakeit.Number(1, 10))),
+	// 		LocationID:  int64(gofakeit.Number(1, 10)),
+	// 		Approved:    true,
+	// 	}
+	// 	db.NewInsert().Model(event).Exec(ctx)
 
-		for j := 0; j < 3; j++ {
-			entranceFee := &models.EntranceFee{
-				EventID: int64(i + 1),
-				Name:    gofakeit.Name(),
-				Price:   int64(gofakeit.Number(1, 100)),
-			}
-			db.NewInsert().Model(entranceFee).Exec(ctx)
-		}
+	// 	for j := 0; j < 3; j++ {
+	// 		entranceFee := &models.EntranceFee{
+	// 			EventID: int64(i + 1),
+	// 			Name:    gofakeit.Name(),
+	// 			Price:   int64(gofakeit.Number(1, 100)),
+	// 		}
+	// 		db.NewInsert().Model(entranceFee).Exec(ctx)
+	// 	}
 
-		for j := 0; j < 3; j++ {
-			categoryToEvent := &models.CategoryToEvent{
-				EventID:    int64(i + 1),
-				CategoryID: int64(gofakeit.Number(1, 6)),
-			}
-			db.NewInsert().Model(categoryToEvent).Exec(ctx)
-		}
-	}
+	// 	for j := 0; j < 3; j++ {
+	// 		categoryToEvent := &models.CategoryToEvent{
+	// 			EventID:    int64(i + 1),
+	// 			CategoryID: int64(gofakeit.Number(1, 6)),
+	// 		}
+	// 		db.NewInsert().Model(categoryToEvent).Exec(ctx)
+	// 	}
+	// }
 
-	for i := 0; i < 100; i++ {
-		user := &models.User{
-			Name:     gofakeit.Name(),
-			Email:    gofakeit.Email(),
-			Password: "$2a$10$Ioo2eOK1UZSJiQ2oh.4Unuvl7MHtrQZLA8WEnEZDytacLXqDFoAXS", // "password"
-			RoleID:   int64(gofakeit.Number(1, 3)),
-		}
-		db.NewInsert().Model(user).Exec(ctx)
+	// for i := 0; i < 100; i++ {
+	// 	user := &models.User{
+	// 		Name:     gofakeit.Name(),
+	// 		Email:    gofakeit.Email(),
+	// 		Password: "$2a$10$Ioo2eOK1UZSJiQ2oh.4Unuvl7MHtrQZLA8WEnEZDytacLXqDFoAXS", // "password"
+	// 		RoleID:   int64(gofakeit.Number(1, 3)),
+	// 	}
+	// 	db.NewInsert().Model(user).Exec(ctx)
 
-		for j := 0; j < 3; j++ {
-			// Fetch random event and fees
-			var event models.Event
-			db.NewSelect().Model(&event).Relation("EntranceFees").Relation("Categories").Where("id = ?", int64(gofakeit.Number(1, 10))).Scan(ctx)
+	// 	for j := 0; j < 3; j++ {
+	// 		// Fetch random event and fees
+	// 		var event models.Event
+	// 		db.NewSelect().Model(&event).Relation("EntranceFees").Relation("Categories").Where("id = ?", int64(gofakeit.Number(1, 10))).Scan(ctx)
 
-			// Create userToEvent with random entrance fee
-			userToEvent := &models.UserToEvent{
-				UserID:        int64(i + 1),
-				EventID:       event.ID,
-				EntranceFeeID: event.EntranceFees[gofakeit.Number(0, len(event.EntranceFees)-1)].ID,
-			}
-			db.NewInsert().Model(userToEvent).Exec(ctx)
-		}
-	}
+	// 		// Create userToEvent with random entrance fee
+	// 		userToEvent := &models.UserToEvent{
+	// 			UserID:        int64(i + 1),
+	// 			EventID:       event.ID,
+	// 			EntranceFeeID: event.EntranceFees[gofakeit.Number(0, len(event.EntranceFees)-1)].ID,
+	// 		}
+	// 		db.NewInsert().Model(userToEvent).Exec(ctx)
+	// 	}
+	// }
 
-	for i := 0; i < 10; i++ {
-		comment := &models.Comment{
-			Text:    gofakeit.LoremIpsumSentence(20),
-			EventID: int64(gofakeit.Number(1, 10)),
-			UserID:  int64(gofakeit.Number(1, 10)),
-		}
-		db.NewInsert().Model(comment).Exec(ctx)
-	}
+	// for i := 0; i < 10; i++ {
+	// 	comment := &models.Comment{
+	// 		Text:    gofakeit.LoremIpsumSentence(20),
+	// 		EventID: int64(gofakeit.Number(1, 10)),
+	// 		UserID:  int64(gofakeit.Number(1, 10)),
+	// 	}
+	// 	db.NewInsert().Model(comment).Exec(ctx)
+	// }
 
-	for i := 0; i < 10; i++ {
-		rating := &models.Rating{
-			Rating:  int64(gofakeit.Number(1, 5)),
-			EventID: int64(gofakeit.Number(1, 10)),
-			UserID:  int64(gofakeit.Number(1, 10)),
-		}
-		db.NewInsert().Model(rating).Exec(ctx)
-	}
+	// for i := 0; i < 10; i++ {
+	// 	rating := &models.Rating{
+	// 		Rating:  int64(gofakeit.Number(1, 5)),
+	// 		EventID: int64(gofakeit.Number(1, 10)),
+	// 		UserID:  int64(gofakeit.Number(1, 10)),
+	// 	}
+	// 	db.NewInsert().Model(rating).Exec(ctx)
+	// }
 
 	/* FOREIGN KEYS */
 	// UserToEvent
